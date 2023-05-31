49d0ea653b30f84cb113c0fec00ba1c53e950e97
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-05-31 14:22:59 +0200

    hbs implement

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 const express = require("express");
-const { path } = require("express/lib/application");
-
+const hbs = require("hbs");
 const app = express();
 
 app.use(express.static("public")); //make everything inside public folder available
-
-//app.get(path, code)
-
-//app.post(path2,()=>{})
+app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
+app.set("view engine", "hbs"); //sets HBS as the template engine
+hbs.registerPartials(__dirname + "/views/partials"); //tells to hbs where is partials folder
 
 app.get("/", (req, res, next) => {
-  res.sendFile(__dirname + "/views/home-page.html");
+  res.render("home-page");
 });
 
 app.get("/contact", (req, res, next) => {
-  res.sendFile(__dirname + "/views/contact-page.html");
+  res.render("contact-page");
+});
+
+app.get("/pizzas/margherita", (req, res, next) => {
+  const dataMargherita = {
+    title: "Margherita",
+    price: 12,
+    recommendedDrink: "beer",
+    imageFile: "/images/margherita.jpg",
+    ingredients: ["mozzarella", "tomato sauce", "basilicum"],
+  };
+  res.render("product", dataMargherita);
+});
+
+app.get("/pizzas/veggie", (req, res, next) => {
+  const dataVeggie = {
+    title: "Veggie",
+    price: 15,
+    recommendedDrink: "power smoothie",
+    imageFile: "/images/veggie.jpg",
+    ingredients: ["cherry tomatoes", "basilicum", "Olives"],
+  };
+  res.render("product", dataVeggie);
+});
+
+app.get("/pizzas/seafood", (req, res, next) => {
+  const dataSeafood = {
+    title: "Seafood",
+    price: 20,
+    recommendedDrink: "white wine",
+    imageFile: "/images/seafood.jpg",
+    ingredients: ["tomato sauce", "garlic", "prawn"],
+  };
+  res.render("product", dataSeafood);
 });
 
 app.listen(80, () => {
